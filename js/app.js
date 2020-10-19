902b9b142968c2cb67e4740eb90c84223dac1408
jideman2k <jideman2k@yandex.ru>
2020-10-19 15:52:15 -0700

    game code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+var x, y, direction;
 let game = [
   [".", ".", ".", ".", ".", ".", ".", ".", "."],
   [".", ".", ".", ".", ".", ".", ".", ".", "."],
@@ -11,13 +12,14 @@ let game = [
 ];
 
 // create an element
-let board = document.createElement("PRE");
+let board = document.createElement("div");
+board.classList.add("board");
 // append the board to body
 document.body.appendChild(board);
 // create a button with an event handle and with textnode & append
 let button = document.createElement("button");
-
-// button.onclick = fire;
+button.classList.add("btn");
+button.onclick = fire;
 
 let btnText = document.createTextNode("Fire!");
 
@@ -25,8 +27,7 @@ document.body.appendChild(button);
 
 button.appendChild(btnText);
 
-// draw the board
-
+// draw the board structure
 function drawBoard() {
   var boardContents = "";
   var i, j;
@@ -42,3 +43,66 @@ function drawBoard() {
 }
 
 board.innerHTML = drawBoard();
+
+x = prompt(
+  "Where would you like to place your ship  Enter an X coordinate:(0-8)"
+);
+y = prompt(
+  "Where would you like to place your ship  Enter an Y coordinate:(0-8)"
+);
+x = Number(x);
+y = Number(y);
+direction = prompt("Place (h)orizontally, (v)ertically");
+
+if (direction[0] == "h") {
+  var c;
+  for (c = x; c < x + 4; c++) {
+    game[y][c] = "#";
+  }
+}
+
+if (direction[0] == "v") {
+  var c;
+  for (c = x; c < x + 4; c++) {
+    game[c][x] = "#";
+  }
+}
+
+board.innerHTML = drawBoard();
+
+function fire() {
+  var fireX = prompt(
+    "Where would you like to fire? Enter an X coordinate: (0-8)"
+  );
+  var fireY = prompt(
+    "Where would you like to fire? Enter an Y coordinate: (0-8)"
+  );
+
+  // converting the inputs to number
+
+  fireX = Number(fireX);
+  fireY = Number(fireY);
+
+  if (game[fireY][fireX] == ".") {
+    alert("You Missed.");
+  } else if (game[fireY][fireX] == "*") {
+    alert("You already hit a ship there");
+  } else {
+    alert("Kaboom! You hit a ship");
+    game[fireY][fireX] == "*";
+    board.innerHTML = drawBoard();
+  }
+
+  var shipfound, i, j;
+  for (i = 0; i < 9; I++) {
+    for (j = 0; j < 9; j++) {
+      if (game[i][j] != "." && game[i][j] != "*") {
+        shipfound = true;
+      }
+    }
+  }
+  if (!shipfound) {
+    alert("All ships have been sunk. well done Captain! Game over");
+    document.body.removeChild(button);
+  }
+}
